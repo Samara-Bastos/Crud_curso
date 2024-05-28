305693372714b54dce51e36b1ae73832dddb7d8d
Samara Bastos <samarabastos.dev@gmail.com>
2024-05-28 15:59:30 -0300

    Alterada a injeção de dependencia

--- src/main/java/crud/curso/controller/AlunoController.java
+++ src/main/java/crud/curso/controller/AlunoController.java
@@ -10,18 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import crud.curso.dto.AlunoRequestDTO;
 import crud.curso.dto.AlunoResponseDTO;
-import crud.curso.service.AlunoServiceImpl;
+import crud.curso.service.AlunoService;
 
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
 
     @Autowired
-    AlunoServiceImpl alunoServiceImpl;
+    AlunoService alunoService;
     
     @PostMapping("/create")
     public ResponseEntity<AlunoResponseDTO> cadastrar(@RequestBody AlunoRequestDTO alunoRequestDTO){
-        AlunoResponseDTO alunoResponseDTO = alunoServiceImpl.cadastrar(alunoRequestDTO);
+        AlunoResponseDTO alunoResponseDTO = alunoService.cadastrar(alunoRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(alunoResponseDTO);
     }
     
