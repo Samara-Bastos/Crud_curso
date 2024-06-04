21f357301df2438d465f46a1f42e751231d591c8
Samara Bastos <samarabastos.dev@gmail.com>
2024-06-04 17:34:17 -0300

    Ajuste no espaçamento

--- src/main/java/crud/curso/repository/AlunoRepository.java
+++ src/main/java/crud/curso/repository/AlunoRepository.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Integer> {
-    
     Optional<Aluno> findByMatricula(String matricula);
 
     @Modifying
